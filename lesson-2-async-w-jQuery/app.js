2f454e5cdc2c38a64c2d5ee9632eca9021e561f2
Imran Khan <imran.khan@kpn.com>
2019-08-01 20:51:07 +0200

    ajax with jquery

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,29 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+      // New York Times API
+      // const articleRequest = new XMLHttpRequest();
+      // articleRequest.onload = addArticles;
+      // articleRequest.open('GET', `https://api.nytimes.com/svc/news/v3/content/all/all.json?q=${searchedForText}&api-key=IL6WzlQkWXfUkp3AtrqvMzG0FRLXWW5e`);
+      // articleRequest.send();
+
+      $.ajax({
+        url: `https://api.nytimes.com/svc/news/v3/content/all/all.json?query=${searchedForText}&api-key=IL6WzlQkWXfUkp3AtrqvMzG0FRLXWW5e`
+      }).done(addArticles);
     });
+
+  function addArticles(data) {
+    let htmlContent = '';
+    const articles = data.results;
+    htmlContent = '<h1>NYT Articles</h1>\n' +
+      '<ul>' + articles.map(article =>
+        `<li class="article">
+            <h2><a href="${article.slug_name}">${article.section} ${article.subsection}</a></h2>
+            <p>${article.byline}</p>
+        </li>`).join('') +
+      '</ul>';
+    responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+  }
+
 })();
