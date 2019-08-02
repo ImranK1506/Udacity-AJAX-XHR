7f92080bd79c19263b6607cbea07d600d7042310
Imran Khan <imran.khan@kpn.com>
2019-08-02 14:58:11 +0200

    fetch api for images and nyt articles

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -8,5 +8,70 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+      fetch(`https://api.nytimes.com/svc/news/v3/content/all/all.json?q=${searchedForText}&api-key=IL6WzlQkWXfUkp3AtrqvMzG0FRLXWW5e`)
+        .then(response => response.json())
+        .then(addArticles);
+
+      // Fetch API method to handle response and receive the json object
+      fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+        {
+          headers: {
+            Authorization: 'Client-ID 6412894fa9683b43370a770c31135621ab516ba348302caa0725d7ec8fd94a1b'
+          }
+        })
+        .then(response => response.json())
+        .then(addImage)
+        .catch(e => requestError(e, ' service'));
+
+        // function addImage(data) {
+        //   console.log(data);
+        //   debugger;
+        // }
+
+      // Error returned if service is unavailable
+      const requestError = (e, part) => {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Error while requesting ${part}</p>`)
+      }
     });
+
+    function addImage (data) {
+      let htmlContent = '';
+      const firstImage = data.results[1];
+
+      if(firstImage) {
+      htmlContent =
+        `<h1>Unsplash Images</h1>
+          <figure>
+            <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>       
+        </figure>`;
+      } else {
+        // If no images available to be returned
+        htmlContent = 'No images to be found'
+      }
+      responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+  function addArticles(data) {
+    let htmlContent = '';
+    const articles = data.results;
+
+    if(articles) {
+    htmlContent = '<h1>NYT Articles</h1>\n' +
+      '<ul>' + articles.map(article =>
+        `<li class="article">
+            <h2><a href="${article.slug_name}">${article.section} ${article.subsection}</a></h2>
+            <p>${article.byline}</p>
+        </li>`).join('') +
+      '</ul>';
+    } else {
+      //  If no article available to be returned
+      htmlContent = 'No articles to be found'
+    }
+    responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+  }
 })();
+
+
